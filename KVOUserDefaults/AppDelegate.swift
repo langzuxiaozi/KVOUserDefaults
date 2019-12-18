d7ba64ac13b6bd84e3a7789bd7352edea7c23478
yanzhao <zhao.yan@moji.com>
2019-12-18 10:16:21 +0800

    first commit

---
+++ KVOUserDefaults/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  KVOUserDefaults
+//
+//  Created by langzuxiaozi on 2019/12/18.
+//  Copyright © 2019 langzuxiaozi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        var storage = Storage()
+
+        storage.isFirstLaunch = true
+        print(storage.isFirstLaunch! ? "true" : "false")
+
+        var observation = storage.$isFirstLaunch.observe { old, new in
+            print(old, new)
+        }
+
+        storage.isFirstLaunch = false
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
